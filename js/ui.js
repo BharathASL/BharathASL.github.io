21d6ef381f4049731b0ef03fb5a1ccd7dd2e0cbb
Bharath Sendhurpandi <bharathasl74185@gmail.com>
2024-10-08 22:26:19 +0530

    Experience added with dummy content

--- js/ui.js
+++ js/ui.js
@@ -1,45 +1,63 @@
 let skills = [];
 
-fetch('js/skills.json')
-    .then(response => {
-        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-        return response.json();
-    })
-    .then(data => {
-        skills = data;
-        loadSkills();
-    })
-    .catch(error => { console.error('Error loading skills.json file', error) });
+$(document).ready(function () {
+    fetch('js/skills.json')
+        .then(response => {
+            if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+            return response.json();
+        })
+        .then(data => {
+            skills = data;
+            loadSkills();
+        })
+        .catch(error => { console.error('Error loading skills.json file', error) });
+
+    $(".filter select").change((event) => {
+        var selection = event.target.value;
+
+        if (selection) {
+            $(`#items > *:not([data-tag~=${selection}])`).addClass("hide");
+            $(`#items > [data-tag~=${selection}]`).removeClass("hide");
+        } else $(`#items > *`).removeClass("hide");
+    });
+
+});
 
 
 function loadSkills() {
     const itemsDiv = $('#items');
 
     itemsDiv.empty();
+
     skills.forEach(skill => {
         const skillDiv = $('<div></div>');
-        const label = $(`<span>${skill.label}</span>`);
 
+        const label = $('<span></span>').text(skill.label);
+
+        let icon;
         if (skill.key) {
-            var icon = $(`<i class="ci ci-${skill.key} ci-6x"></i>`);
-            skillDiv.append(icon);
+            icon = $('<i></i>').addClass(`ci ci-${skill.key} ci-6x`);
         } else if (skill.svg) {
-            var icon = skill.svg;
-            skillDiv.append(icon);
-            skillDiv.children("svg").addClass("ci-6x");
+            icon = $(skill.svg);
+            icon.addClass('ci-6x');
         } else if (skill["svg-url"]) {
-            var icon = $(`<i class="ci-6x"></i>`);
-            skillDiv.append(icon);
-            skillDiv.children("i").css("content", `url(${skill["svg-url"]})`);
+            icon = $('<i></i>').addClass('ci-6x');
+            icon.css('content', `url(${skill["svg-url"]})`);
         }
+
+        if (icon) skillDiv.append(icon);
 
         if (skill.class) {
-            const child = skillDiv.children();
-
-            skill.class.forEach(e => child.addClass(e));
+            skill.class.forEach(className => {
+                skillDiv.children().addClass(className);
+            });
         }
 
+        if (skill.tag) skillDiv.attr('data-tag', skill.tag.join(" "));
+
         skillDiv.append(label);
+
         itemsDiv.append(skillDiv);
     });
 }
